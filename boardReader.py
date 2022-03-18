9cdfdd6a4418d2ae3b8eed8891f3f832a48be8a7
Carter Henry <cwhenry113@selu.edu>
2022-03-17 19:11:16 -0500

    3/17 work

--- boardReader.py
+++ boardReader.py
@@ -4,26 +4,19 @@
 # If you need anything related to this file, talk to Carter.
 
 # Given an image (Working filename/path is just "boardImage.jpg"), processing occurs in steps:
-# Step 1: Convert image to greyscale, then blur
-# Step 2: Detect outline of the chess board
-# Step 3: Crop along that outline
-# Step 4: Sequentially detect the first threshold, going low x to high x before low y to high y
-# Step 5: Determine the occupation status on the lower x side of the threshold
-# Step 6: Record the occupation status in the array output[n%8][n/8][occupation]
-# Step 7: Repeat steps 4-6, ignoring the first n detections. Continue until n = 64
-
-# Possible ways to improve accuracy: 
-# Taking multiple pictures per step and taking the clearest
-# Using R, G, and B masks and comparing seperately as opposed to a single greyscale mask
-
+# Step 1: Convert image to greyscale, then blur. Use thresholding to make black/white
+# Step 2 (TO-DO): Detect outline of the chess board and crop along that outline
+# Step 3: Sequentially detect the first threshold, going high y to low y, low x to high x
+# Step 4: Determine the occupation status on the lower x side of the threshold
+# Step 5: Repeat steps 4-6, until all squares have been recognized
 
 import sys
-import numpy
+import numpy as np
 import cv2 as cv
-from cv2 import THRESH_BINARY
+from imutils import contours
 
 # Image input
-img = cv.imread("devImages/inkedCroppedBoard.jpg")
+img = cv.imread("devImages/inkedCroppedBoardSmall.jpg")
 oimg = img
 cv.imwrite("imageOut/input.jpg", img)
 
@@ -33,16 +26,59 @@ img = cv.GaussianBlur(img, (39,39), 6, 6)
 cv.imwrite("imageOut/grayscale.jpg", img)
 
 # Thresholding
-img = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+img = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 11, 2)
 cv.imwrite("imageOut/threshBinary.jpg", img)
 
 # Contours
-contours, hierarchy = cv.findContours(image = img, mode = cv.RETR_TREE, method = cv.CHAIN_APPROX_SIMPLE)
-cv.drawContours(image = oimg, contours = contours, contourIdx = -1, color = (0, 0, 255), thickness = 2, lineType = cv.LINE_AA)
+cnts = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+cv.drawContours(image= oimg, contours= cnts, contourIdx= -1, color= (0, 0, 255), thickness= 2, lineType= cv.LINE_AA)
 cv.imwrite("imageOut/countours.jpg", oimg)
-i = 0
-while i < len(contours):
-    print(str(contours[i]))
+for c in cnts:
+    area = cv.contourArea(c)
+    if area < 1000:
+        cv.drawContours(img, [c], -1, (0,0,0), -1)
+
+# Fix boundaries
+vertical_kernel = cv.getStructuringElement(cv.MORPH_RECT, (1,5))
+img = cv.morphologyEx(img, cv.MORPH_CLOSE, vertical_kernel, iterations=9)
+horizontal_kernel = cv.getStructuringElement(cv.MORPH_RECT, (5,1))
+img = cv.morphologyEx(img, cv.MORPH_CLOSE, horizontal_kernel, iterations=4)
+
+# Sort top -> bottom and left -> right
+invert = 255 - img
+cnts = cv.findContours(invert, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+if len(cnts) == 2:
+    cnts = cnts[0] 
+else:
+    cnts = cnts[1]
+(cnts, _) = contours.sort_contours(cnts, method="top-to-bottom")
+
+chess_rows = []
+row = []
+for (i, c) in enumerate(cnts, 1):
+    area = cv.contourArea(c)
+    if area > 50000:
+        row.append(c)
+        if i % 9 == 0:
+            (cnts, _) = contours.sort_contours(row, method="left-to-right")
+            chess_rows.append(cnts)
+            row = []
+
+# Iterate through boxes
+for row in chess_rows:
+    for c in row:
+        mask = np.zeros(img.shape, dtype=np.uint8)
+        cv.drawContours(mask, [c], -1, (255,255,255), -1)
+        result = cv.bitwise_and(image, mask)
+        result[mask==0] = 255
+        cv.imshow('result', result)
+        cv.waitKey(175)
+
+cv.imshow('img', img)
+cv.imshow('invert', invert)
+cv.waitKey()
+
 #Image display, for development purposes
 #cv.imshow("Image", img)
 #cv.waitKey(0)
---
+++ boardReaderExperi.py
@@ -0,0 +1,84 @@
+# boardReader is intended to take the most recent image of the game board and convert it an array.
+# The array is 8x8x3, with dimensions corresponding to x coordinate, y coordinate, and occupation.
+# Occupation codes are 0, 1, and 2 corresponding to empty, white piece, and black piece respectively.
+# If you need anything related to this file, talk to Carter.
+
+# Given an image, processing occurs in steps:
+# Step 1: Convert image to greyscale, then blur. Use thresholding to make black/white
+# Step 2 (TO-DO): Detect outline of the chess board and crop along that outline
+# Step 3: Sequentially detect the first threshold, going high y to low y, low x to high x
+# Step 4: Determine the occupation status on the lower x side of the threshold
+# Step 5: Repeat steps 3 + 4 until all squares have been recognized
+
+import cv2
+from imutils import contours
+import numpy as np
+
+# Load image, grayscale, and adaptive threshold
+image = cv2.imread('devImages\inkedCroppedBoardSmall.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,2)
+
+# Filter out all noise to isolate only boxes
+cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+for c in cnts:
+    area = cv2.contourArea(c)
+    if area < 1000:
+        cv2.drawContours(thresh, [c], -1, (0,0,0), -1)
+
+# Fix horizontal and vertical lines
+vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1,5))
+thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, vertical_kernel, iterations=9)
+horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,1))
+thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, horizontal_kernel, iterations=4)
+
+# Sort by top to bottom and each row by left to right
+invert = 255 - thresh
+cnts = cv2.findContours(invert, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+(cnts, _) = contours.sort_contours(cnts, method="top-to-bottom")
+
+chess_rows = []
+row = []
+for (i, c) in enumerate(cnts, 1):
+    area = cv2.contourArea(c)
+    if area < 50000:
+        row.append(c)
+        if i % 9 == 0:  
+            (cnts, _) = contours.sort_contours(row, method="left-to-right")
+            chess_rows.append(cnts)
+            row = []
+
+# Image detection within box
+sqList = [[image]*8]*8
+sqLT = 0
+sqLC = 0
+for row in chess_rows:
+    for c in row:
+        mask = np.zeros(image.shape, dtype=np.uint8)
+        cv2.drawContours(mask, [c], -1, (255,255,255), -1)
+        result = cv2.bitwise_and(image, mask)
+        result[mask==0] = 255
+        if (sqLC > 7):
+            sqLC = 0
+            sqLT += 1
+        if (sqLT != 8):
+            sqList[sqLC][7 - sqLT]
+            sqLC += 1
+        #cv2.imshow('result', result)
+        #cv2.waitKey(175)
+temps = ["templates\pawn.jpg", "templates\king.jpg", "templates\knight.jpg", "templates\qbishop.jpg", "templates\qrook.jpg", "templates\queen.jpg"]
+oList = [[]*8]*8
+for x in range(len(oList)):
+    for y in range(len(oList[x])):
+        for t in range(len(temps)):
+            temp = cv2.imread(temps[t])
+            if (cv2.matchTemplate(sqList[x][y], temp, cv2.TM_CCOEFF_NORMED) > 0.8):
+                oList[x][y] = 1
+            else:
+                oList[x][y] = 0
+
+cv2.imshow('thresh', thresh)
+cv2.imshow('invert', invert)
+cv2.waitKey()
