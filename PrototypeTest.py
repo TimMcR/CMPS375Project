f9cd23dcc8bb1b7ef6a9a3da054eb0e1388ad63a
TimMcR <31673072+TimMcR@users.noreply.github.com>
2022-04-07 12:37:04 -0500

    Updated PrototypeTest for testing libraries

--- PrototypeTest.py
+++ PrototypeTest.py
@@ -1,7 +1,7 @@
 import boardReader as cv
 import theboard
 
-# Image output from camera
+# Image output file from camera
 imgPath = '...'
 
 # Board Reader library determines current position of chess pieces
@@ -11,22 +11,34 @@ newPositions = cv.getPositions()
 # Board library determines if a valid move has been made and other chess logic
 board = theboard.Board()
 board.checkMove(newPositions)
+
+# Move was valid
 if board.isValidMove:
     print('Valid move')
+
+    # Get necessary info
     currentColor = board.blackorwhite
+
+    startSquare = board.str[0:2]
+    endSquare = board.str[2:4]
+
     # Check for preliminary conditions
     if board.isCheck:
         print('King is in danger')
 
-    if board.isCheckMate:
+    elif board.isCheckMate:
         print('King has been captured')
-    else:
-        # Move pieces
-        if currentColor == 0 and board.whitecastled:
-            print('White has castled')
-        if currentColor == 1 and board.blackcastled:
-            print('Black has castled')
 
+    # Move pieces
+    # White move
+    elif currentColor == 0:
+        print('Move white piece')
+        # movePiecesAuto(startSquare, endSquare)
 
+    elif currentColor == 1:
+        print('Move black piece')
+        # movePiecesAuto(startSquare, endSquare)
+
+# Move was invalid
 else:
     print('Invalid move')
