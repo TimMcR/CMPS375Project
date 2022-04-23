0c5aab227836aea72380f005b479b08baad20456
Carter Henry <cwhenry113@selu.edu>
2022-04-23 15:47:33 -0500

    Cropping and multi-template matching

--- boardReader.py
+++ boardReader.py
@@ -1,116 +1,159 @@
+#<<<<<<< HEAD
+
 # boardReader is intended to take the most recent image of the game board and convert it an array.
 # The array is 8x8x3, with dimensions corresponding to x coordinate, y coordinate, and occupation.
-# Occupation codes are 0, 1, and 2 corresponding to empty, white piece, and black piece respectively.
-# If you need anything related to this file, talk to Carter.
 
-# Given an image (Working filename/path is just "boardImage.jpg"), processing occurs in steps:
-# Step 1: Convert image to greyscale, then blur. Use thresholding to make black/white
-# Step 2 (TO-DO): Detect outline of the chess board and crop along that outline
-# Step 3: Sequentially detect the first threshold, going high y to low y, low x to high x
-# Step 4: Determine the occupation status on the lower x side of the threshold
-# Step 5: Repeat steps 4-6, until all squares have been recognized
+# boardReader is intended to take the most recent image of the game board and convert it an array.
+# The array is 8x8x3, with dimensions corresponding to x coordinate, y coordinate, and occupation.
+# Assumes that provided image is 1280x1280 pixels
 
-import sys
-import numpy as np
-import cv2 as cv
+import cv2
 from imutils import contours
+import numpy as np
 
-#<<<<<<< HEAD
-# Image input
-img = cv.imread("devImages/inkedCroppedBoardSmall.jpg")
-oimg = img
-cv.imwrite("imageOut/input.jpg", img)
 
-# Colorspace + Blurring
-img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-img = cv.GaussianBlur(img, (39,39), 6, 6)
-cv.imwrite("imageOut/grayscale.jpg", img)
-
-# Thresholding
-img = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 11, 2)
-cv.imwrite("imageOut/threshBinary.jpg", img)
-
-# Contours
-cnts = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+# ++++++++++++++++++++++++++++++++++++++++++++
+# +++++++Cropping game board from image+++++++
+# ++++++++++++++++++++++++++++++++++++++++++++
+# Load image, grayscale, and adaptive threshold
+image = cv2.imread('devImages\decroppedBoard.png')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,12)
+cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#Make all contours top level
 cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-cv.drawContours(image= oimg, contours= cnts, contourIdx= -1, color= (0, 0, 255), thickness= 2, lineType= cv.LINE_AA)
-cv.imwrite("imageOut/countours.jpg", oimg)
+#Use area and number of sides of each contour to search for the board and white out all else
+for (i, c) in enumerate(cnts, 1):
+    area = cv2.contourArea(c)
+    #If area is > 300,000 (~547x547 pixels, or ~20% of the image's total area), to ensure we're not picking up individual squares
+    if area > 300000:
+        #If area is < 1,000,000 (1000x1000 pixels, or ~60% of the image's total area), to ensure it's not a just a table or something
+        if area < 1000000:
+            approx = cv2.approxPolyDP(c, 0.01 * cv2.arcLength(c,True), True)
+            print("Side count: ", str(len(approx)))
+            #If the shape is a quadrilateral
+            if len(approx) == 4:
+                n = approx.ravel()
+                q = 0
+                cropXMax = 0
+                cropYMax = 0
+                cropXMin = 10000
+                cropYMin = 10000
+                for j in n:
+                    if (q % 2 == 0):
+                        if (n[q] > cropXMax):
+                            cropXMax = n[q]
+                        if (n[q] < cropXMin):
+                            cropXMin = n[q]
+                        if (n[q+1] > cropYMax):
+                            cropYMax = n[q+1]
+                        if (n[q+1] < cropYMin):
+                            cropYMin = n[q+1]
+                    q = q + 1
+                #cv2.line(image,(cropXMin,cropYMin), (cropXMax,cropYMax), (255,0,0), 10)
+                #cv2.imwrite("imageOut\cropDimensions.png", image)
+                image = image[cropYMin:cropYMax, cropXMin:cropXMax]
+                image = cv2.resize(image, (1000,1000))
+                cv2.imwrite("imageOut\cropped.png", image)
+                print("Image cropped")
+                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,15,12)
+                cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++
+# +++++Splitting cropped image into boxes+++++
+# ++++++++++++++++++++++++++++++++++++++++++++
+#Thicken lines
 for c in cnts:
-    area = cv.contourArea(c)
-    if area < 1000:
-        cv.drawContours(img, [c], -1, (0,0,0), -1)
-
-# Fix boundaries
-vertical_kernel = cv.getStructuringElement(cv.MORPH_RECT, (1,5))
-img = cv.morphologyEx(img, cv.MORPH_CLOSE, vertical_kernel, iterations=9)
-horizontal_kernel = cv.getStructuringElement(cv.MORPH_RECT, (5,1))
-img = cv.morphologyEx(img, cv.MORPH_CLOSE, horizontal_kernel, iterations=4)
-
-# Sort top -> bottom and left -> right
-invert = 255 - img
-cnts = cv.findContours(invert, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-if len(cnts) == 2:
-    cnts = cnts[0] 
-else:
-    cnts = cnts[1]
+    area = cv2.contourArea(c)
+    if area < 7000:
+        cv2.drawContours(thresh, [c], -1, (0,0,0), -1)
+# Fix horizontal and vertical lines
+vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1,5))
+thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, vertical_kernel, iterations=9)
+horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,1))
+thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, horizontal_kernel, iterations=4)
+#Sort the contours from top-to-bottom, take the top 8, then sort them left-to-right
+invert = 255 - thresh
+cnts = cv2.findContours(invert, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 (cnts, _) = contours.sort_contours(cnts, method="top-to-bottom")
-
 chess_rows = []
 row = []
 for (i, c) in enumerate(cnts, 1):
-    area = cv.contourArea(c)
-    if area > 50000:
-        row.append(c)
-        if i % 9 == 0:
-            (cnts, _) = contours.sort_contours(row, method="left-to-right")
-            chess_rows.append(cnts)
-            row = []
+    area = cv2.contourArea(c)
+    if area < 90000:
+        if area > 5000:
+            row.append(c)
+            if i % 8 == 0:  
+                (cnts, _) = contours.sort_contours(row, method="left-to-right")
+                chess_rows.append(cnts)
+                row = []
 
-# Iterate through boxes
+
+# ++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++Image detection within boxes++++++++
+# ++++++++++++++++++++++++++++++++++++++++++++
+# Note: High accuracy template lists have been commented out as they have proven cumbersome time-wise.
+# Abridged lists have been provided for live demonstration. Restoring full accuracy will massively increase required time.
+compositeResult = np.zeros(image.shape, dtype=np.uint8)
+oList = []
+# Making list of templates to match
+#templateList120 = []
+templateList100 = []
+#templateList80 = []
+templateList60 = []
+fileNames1 = ["king", "queen", "qbishop", "qrook", "knight", "pawn"]
+#fileNames2 = ["0d", "9d", "18d", "27d", "36d", "45d", "54d", "63d", "72d", "81d"]
+fileNames2 = ["0d", "9d"]
+for fn1 in fileNames1:
+    for fn2 in fileNames2:
+        templateList100.append(cv2.imread("templates\\"+fn1+fn2+".png"))
+for tmps in templateList100:
+    #templateList120.append(cv2.resize(tmps, (120,120)))
+    #templateList80.append(cv2.resize(tmps, (80,80)))
+    templateList60.append(cv2.resize(tmps, (60,60)))
+#tmpsLists = [templateList120, templateList100, templateList80, templateList60]
+tmpsLists = [templateList100, templateList60]
+#Matching each individual square against all templates until a good match is found or all templates have been examined
 for row in chess_rows:
     for c in row:
-        mask = np.zeros(img.shape, dtype=np.uint8)
-        cv.drawContours(mask, [c], -1, (255,255,255), -1)
-        result = cv.bitwise_and(image, mask)
-        result[mask==0] = 255
-        cv.imshow('result', result)
-        cv.waitKey(175)
-
-cv.imshow('img', img)
-cv.imshow('invert', invert)
-cv.waitKey()
-
-#Image display, for development purposes
-#cv.imshow("Image", img)
-#cv.waitKey(0)
-#=======
-# From Ramsey: I just encapsulated everything into the read method for simplicity
-def read(imagePath):
-    # Image input
-    img = cv.imread(imagePath)
-    oimg = img
-    cv.imwrite("imageOut/input.jpg", img)
-
-    # Colorspace + Blurring
-    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    img = cv.GaussianBlur(img, (39,39), 6, 6)
-    cv.imwrite("imageOut/grayscale.jpg", img)
-
-    # Thresholding
-    img = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
-    cv.imwrite("imageOut/threshBinary.jpg", img)
-
-    # Contours
-    contours, hierarchy = cv.findContours(image = img, mode = cv.RETR_TREE, method = cv.CHAIN_APPROX_SIMPLE)
-    cv.drawContours(image = oimg, contours = contours, contourIdx = -1, color = (0, 0, 255), thickness = 2, lineType = cv.LINE_AA)
-    cv.imwrite("imageOut/countours.jpg", oimg)
-    i = 0
-    while i < len(contours):
-        print(str(contours[i]))
-    #Image display, for development purposes
-    #cv.imshow("Image", img)
-    #cv.waitKey(0)
-
+        # Isolate the square
+        mask = np.zeros(image.shape, dtype=np.uint8)
+        cv2.drawContours(mask, [c], -1, (255,255,255), -1)
+        result = cv2.bitwise_and(image, mask)
+        # Match templates
+        bestMatch = 0
+        for tmpsList in tmpsLists:
+            for tmps in tmpsList:
+                if bestMatch > 0.3:
+                    continue
+                currMatch = cv2.matchTemplate(result, tmps, cv2.TM_CCOEFF_NORMED)
+                (__, maxVal, __, __) = cv2.minMaxLoc(currMatch)
+                if maxVal > bestMatch:
+                    bestMatch = maxVal
+        print(str(bestMatch))
+        # Mark space as occupied if a good match was found
+        if bestMatch > 0.3:
+            oList.append(1)
+        else:
+            oList.append(0)
+        cv2.imshow('result', result)
+        compositeResult = compositeResult + result
+        cv2.waitKey(50)
+cv2.imshow('Composite Result', compositeResult)
+cv2.waitKey(1000)
+cv2.imwrite('imageOut\Othresh.png', thresh)
+cv2.imwrite('imageOut\invert.png', invert)
+cv2.imwrite('imageOut\composite.png', compositeResult)
+listSizeGap = 64 - len(oList)
+while listSizeGap > 0:
+    oList.append(0)
+    listSizeGap = listSizeGap - 1
+aList = np.array(oList).reshape(8,8)
+print(str(aList))
 # Un-comment to run
 #read("devImages/inkedCroppedBoard.jpg")
 #>>>>>>> 8daca53742d8e81d7b040fea98e0324cf8069057
--- boardReaderExperi.py
+++ boardReaderExperi.py
@@ -1,38 +1,75 @@
 # boardReader is intended to take the most recent image of the game board and convert it an array.
 # The array is 8x8x3, with dimensions corresponding to x coordinate, y coordinate, and occupation.
+# Assumes that provided image is 1280x1280 pixels
 
 import cv2
 from imutils import contours
 import numpy as np
 
+
+# ++++++++++++++++++++++++++++++++++++++++++++
+# +++++++Cropping game board from image+++++++
+# ++++++++++++++++++++++++++++++++++++++++++++
 # Load image, grayscale, and adaptive threshold
-image = cv2.imread('devImages\croppedRealBoard4.png')
+image = cv2.imread('devImages\decroppedBoard.png')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,12)
-
-
-# Filter out all noise to isolate only boxes
 cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#Make all contours top level
 cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+#Use area and number of sides of each contour to search for the board and white out all else
+for (i, c) in enumerate(cnts, 1):
+    area = cv2.contourArea(c)
+    #If area is > 300,000 (~547x547 pixels, or ~20% of the image's total area), to ensure we're not picking up individual squares
+    if area > 300000:
+        #If area is < 1,000,000 (1000x1000 pixels, or ~60% of the image's total area), to ensure it's not a just a table or something
+        if area < 1000000:
+            approx = cv2.approxPolyDP(c, 0.01 * cv2.arcLength(c,True), True)
+            print("Side count: ", str(len(approx)))
+            #If the shape is a quadrilateral
+            if len(approx) == 4:
+                n = approx.ravel()
+                q = 0
+                cropXMax = 0
+                cropYMax = 0
+                cropXMin = 10000
+                cropYMin = 10000
+                for j in n:
+                    if (q % 2 == 0):
+                        if (n[q] > cropXMax):
+                            cropXMax = n[q]
+                        if (n[q] < cropXMin):
+                            cropXMin = n[q]
+                        if (n[q+1] > cropYMax):
+                            cropYMax = n[q+1]
+                        if (n[q+1] < cropYMin):
+                            cropYMin = n[q+1]
+                    q = q + 1
+                #cv2.line(image,(cropXMin,cropYMin), (cropXMax,cropYMax), (255,0,0), 10)
+                #cv2.imwrite("imageOut\cropDimensions.png", image)
+                image = image[cropYMin:cropYMax, cropXMin:cropXMax]
+                image = cv2.resize(image, (1000,1000))
+                cv2.imwrite("imageOut\cropped.png", image)
+                print("Image cropped")
+                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,15,12)
+                cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++
+# +++++Splitting cropped image into boxes+++++
+# ++++++++++++++++++++++++++++++++++++++++++++
+#Thicken lines
 for c in cnts:
     area = cv2.contourArea(c)
-    if area < 1000:
+    if area < 7000:
         cv2.drawContours(thresh, [c], -1, (0,0,0), -1)
-
-# Use a mask to ensure we only look at the chess board
-#for (i, c) in enumerate(cnts, 1):
-#    area = cv2.contourArea(c)
-#    if area > 30000:
-#        cropMask = np.zeros(image.shape, dtype=np.uint8)
-#        cv2.drawContours(cropMask, [c], -1, (255,255,255), -1)
-#        image = cv2.bitwise_and(cropMask, image)
-
 # Fix horizontal and vertical lines
 vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1,5))
 thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, vertical_kernel, iterations=9)
 horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,1))
 thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, horizontal_kernel, iterations=4)
-
 #Sort the contours from top-to-bottom, take the top 8, then sort them left-to-right
 invert = 255 - thresh
 cnts = cv2.findContours(invert, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -42,8 +79,8 @@ chess_rows = []
 row = []
 for (i, c) in enumerate(cnts, 1):
     area = cv2.contourArea(c)
-    if area < 30000:
-        if area > 4000:
+    if area < 90000:
+        if area > 5000:
             row.append(c)
             if i % 8 == 0:  
                 (cnts, _) = contours.sort_contours(row, method="left-to-right")
@@ -51,35 +88,50 @@ for (i, c) in enumerate(cnts, 1):
                 row = []
 
 
-# Image detection within box
+# ++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++Image detection within boxes++++++++
+# ++++++++++++++++++++++++++++++++++++++++++++
+# Note: High accuracy template lists have been commented out as they have proven cumbersome time-wise.
+# Abridged lists have been provided for live demonstration. Restoring full accuracy will massively increase required time.
 compositeResult = np.zeros(image.shape, dtype=np.uint8)
-#temps = [cv2.imread("templates\pawnSmall.png"), cv2.imread("templates\kingSmall.png"), cv2.imread("templates\knightSmall.png"), cv2.imread("templates\qbishopSmall.png"), cv2.imread("templates\qrookSmall.png"), cv2.imread("templates\queenSmall.png")]
 oList = []
-phTemp = cv2.imread("templates\knightSmall.png")
-
+# Making list of templates to match
+#templateList120 = []
+templateList100 = []
+#templateList80 = []
+templateList60 = []
+fileNames1 = ["king", "queen", "qbishop", "qrook", "knight", "pawn"]
+#fileNames2 = ["0d", "9d", "18d", "27d", "36d", "45d", "54d", "63d", "72d", "81d"]
+fileNames2 = ["0d", "9d"]
+for fn1 in fileNames1:
+    for fn2 in fileNames2:
+        templateList100.append(cv2.imread("templates\\"+fn1+fn2+".png"))
+for tmps in templateList100:
+    #templateList120.append(cv2.resize(tmps, (120,120)))
+    #templateList80.append(cv2.resize(tmps, (80,80)))
+    templateList60.append(cv2.resize(tmps, (60,60)))
+#tmpsLists = [templateList120, templateList100, templateList80, templateList60]
+tmpsLists = [templateList100, templateList60]
+#Matching each individual square against all templates until a good match is found or all templates have been examined
 for row in chess_rows:
     for c in row:
         # Isolate the square
         mask = np.zeros(image.shape, dtype=np.uint8)
         cv2.drawContours(mask, [c], -1, (255,255,255), -1)
         result = cv2.bitwise_and(image, mask)
-        #Do template matching in the square
-        #match = 0
-        #for t in temps:
-        #    temp = cv2.imread(t)
-        #    templateResult = cv2.matchTemplate(result, temp, cv2.TM_CCOEFF_NORMED)
-        #    (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(templateResult)
-        #    if maxVal > 0.5:
-        #        print("Match found")
-        #        match = 1
-        #if match == 1:
-        #    oList.append(1)
-        #else:
-        #    oList.append(0)
-        templateResult = cv2.matchTemplate(result, phTemp, cv2.TM_CCOEFF_NORMED)
-        (__, maxVal, __, __) = cv2.minMaxLoc(templateResult)
-        print(str(maxVal))
-        if maxVal > 0.3:
+        # Match templates
+        bestMatch = 0
+        for tmpsList in tmpsLists:
+            for tmps in tmpsList:
+                if bestMatch > 0.3:
+                    continue
+                currMatch = cv2.matchTemplate(result, tmps, cv2.TM_CCOEFF_NORMED)
+                (__, maxVal, __, __) = cv2.minMaxLoc(currMatch)
+                if maxVal > bestMatch:
+                    bestMatch = maxVal
+        print(str(bestMatch))
+        # Mark space as occupied if a good match was found
+        if bestMatch > 0.3:
             oList.append(1)
         else:
             oList.append(0)
@@ -91,5 +143,9 @@ cv2.waitKey(1000)
 cv2.imwrite('imageOut\Othresh.png', thresh)
 cv2.imwrite('imageOut\invert.png', invert)
 cv2.imwrite('imageOut\composite.png', compositeResult)
+listSizeGap = 64 - len(oList)
+while listSizeGap > 0:
+    oList.append(0)
+    listSizeGap = listSizeGap - 1
 aList = np.array(oList).reshape(8,8)
 print(str(aList))
